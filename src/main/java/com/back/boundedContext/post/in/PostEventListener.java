d504ffbf3609b7de84ad4a70b21549967b5e69ab
samdonggg <mjjr0424@naver.com>
2026-09-18 14:19:53 +0900

    [0020] 회원정보 수정 시 PostMember 테이블에 동기화

--- src/main/java/com/back/boundedContext/post/in/PostEventListener.java
+++ src/main/java/com/back/boundedContext/post/in/PostEventListener.java
@@ -1,8 +1,8 @@
 package com.back.boundedContext.post.in;
 
-
 import com.back.boundedContext.post.app.PostFacade;
 import com.back.shared.member.event.MemberJoinedEvent;
+import com.back.shared.member.event.MemberModifiedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +19,12 @@ public class PostEventListener {
     @TransactionalEventListener(phase = AFTER_COMMIT)
     @Transactional(propagation = REQUIRES_NEW)
     public void handle(MemberJoinedEvent event) {
+        postFacade.syncMember(event.getMember());
+    }
+
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(MemberModifiedEvent event) {
         postFacade.syncMember(event.getMember());
     }
 }
