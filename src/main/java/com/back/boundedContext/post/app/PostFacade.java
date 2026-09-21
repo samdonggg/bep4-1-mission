38d8211250d11be38f9d78b72d891926e3bb0966
samdonggg <mjjr0424@naver.com>
2026-09-21 11:46:09 +0900

    [0031] 글 조회를 위해 PostApiClient 구현

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class PostFacade {
@@ -39,5 +41,10 @@ public class PostFacade {
     @Transactional(readOnly = true)
     public Optional<PostMember> findMemberByUsername(String username) {
         return postSupport.findMemberByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Post> findByOrderByIdDesc() {
+        return postSupport.findByOrderByIdDesc();
     }
 }
